b2ddc2ed8f6aa94d74821bc32964553cd9f78807
EC2 Default User <ec2-user@ip-172-31-7-111.ec2.internal>
2023-01-06 15:42:55 +0000

    commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,9 +25,16 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+        render "edit"
+    else
+        redirect_to books_path
+    end
   end
   
   def update
+    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+        render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
   end
   
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "You have updated user successfully."
     else
