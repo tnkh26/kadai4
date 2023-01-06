4fc96ce7e24e856e448cf08d3ad3992a02b46076
EC2 Default User <ec2-user@ip-172-31-7-111.ec2.internal>
2023-01-06 15:19:09 +0000

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    books_path(resource)
+    user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
