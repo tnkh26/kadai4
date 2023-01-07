d243b823d7d0bd083ea9a45574d7b485a4fd2b76
EC2 Default User <ec2-user@ip-172-31-7-111.ec2.internal>
2023-01-07 04:14:29 +0000

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,20 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    flash[:notice] ="Welcome! You have signed up successfully."
-    user_path(current_user)
+    if current_user
+      flash[:notice] = "Signed in successfully."
+      user_path(current_user)
+    else
+      flash[:notice] = "Welcome! You have signed up successfully."
+     user_path(current_user)
+    end
   end
 
   def after_sign_out_path_for(resource)
     flash[:notice] ="Signed out successfully."
     root_path
   end
-
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
