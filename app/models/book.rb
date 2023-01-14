5ebbfe453d591fd9e1c9b215c56f9a7db3d23fe3
EC2 Default User <ec2-user@ip-172-31-7-111.ec2.internal>
2023-01-14 04:45:26 +0000

    commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,17 @@
 class Book < ApplicationRecord
   
     belongs_to :user
+    has_many :favorites, dependent: :destroy
+    has_many :book_comments, dependent: :destroy
     has_one_attached :profile_image
     
     validates :title,presence:true
 	  validates :body,presence:true,length:{maximum:200}
-	  
+
+def favorited_by?(user)
+  favorites.exists?(user_id: user.id)
+end
+
 def get_profile_image(width, height)
   unless profile_image.attached?
     file_path = Rails.root.join('app/assets/images/no_image.jpg')
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/about"=>"homes#about"
   resources :users
-  resources :books 
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   root :to =>"homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
